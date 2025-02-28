f94c888b37fb5f18ae85c7ca339add2d114367f8
Carrington-dev <crn96m@gmail.com>
2025-02-28 09:22:53 +0200

    Added test 1 for home

--- test_app.py
+++ test_app.py
@@ -1,2 +1,12 @@
 from app import app
 from pytest import fixture
+
+@fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'Hi Carrington, my name is Wonderful'}
